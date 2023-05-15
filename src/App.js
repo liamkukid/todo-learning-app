45d5ac64ab40a08160b2d004742dd8024348525f
Kulik Dzmitry <liamkukid@gmail.com>
2023-05-15 11:22:37 +0200

    module scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className={style.App_header}>
+        <img src={logo} className={style.App_logo} alt="logo" />
       </header>
     </div>
   );

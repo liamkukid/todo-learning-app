864b7a2d6b9d7cc63d8923fc6e612b1a590d4b5f
Kulik Dzmitry <liamkukid@gmail.com>
2023-05-25 16:59:28 +0200

    List tests

--- src/__tests__/List.test.js
+++ src/__tests__/List.test.js
@@ -1,10 +1,54 @@
 import renderer from 'react-test-renderer';
-
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import todosReducer from '../features/todosSlice';
 import List from '../Components/TodosList/List';
 
-jest.mock('react-redux');
+it('renders correctly with no todos', () => {
+  const store = configureStore({
+    reducer: {
+      todos: todosReducer,
+    },
+    preloadedState: {
+      todos: [],
+    },
+  });
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <List />
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-it('renders correctly for empty List', () => {
-  const tree = renderer.create(<List />);
+it('renders correctly with todos', () => {
+  const store = configureStore({
+    reducer: {
+      todos: todosReducer,
+    },
+    preloadedState: {
+      todos: [
+        {
+          title: 'todo 1',
+          done: true,
+          id: 'todo_1_id',
+        },
+        {
+          title: 'todo 2',
+          done: false,
+          id: 'todo_2_id',
+        },
+      ],
+    },
+  });
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <List />
+      </Provider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });

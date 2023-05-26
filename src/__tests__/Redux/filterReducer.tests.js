e71d75d67a30de77dca5806cead4420c984c964d
Kulik Dzmitry <liamkukid@gmail.com>
2023-05-26 10:41:22 +0200

    tests for filter reducer

---
+++ src/__tests__/Redux/filterReducer.tests.js
@@ -0,0 +1,22 @@
+import reducer, {
+  filterActive,
+  showAll,
+  filterCompleted,
+} from '../../features/filterSlice';
+import { SHOW_ALL, FILTER_COMPLETED, FILTER_ACTIVE } from '../../globalConst';
+
+const previousState = '';
+
+describe('filterReducer should', () => {
+  it('cnange filter state to SHOW_ALL', () => {
+    expect(reducer(previousState, showAll())).toEqual(SHOW_ALL);
+  });
+
+  it('cnange filter state to FILTER_COMPLETED', () => {
+    expect(reducer(previousState, filterCompleted())).toEqual(FILTER_COMPLETED);
+  });
+
+  it('cnange filter state to FILTER_ACTIVE', () => {
+    expect(reducer(previousState, filterActive())).toEqual(FILTER_ACTIVE);
+  });
+});

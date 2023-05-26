a570ea29edaad521b520ad9815e1cbbb70aeb1cf
Kulik Dzmitry <liamkukid@gmail.com>
2023-05-26 10:24:43 +0200

    cover todosSlice with tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-export const mockTodoUuid = '14e520cd-63f2-485e-9b3b-072ce068554e';
-// eslint-disable-next-line no-undef
-jest.mock('uuid', () => ({ v4: () => mockTodoUuid }));
